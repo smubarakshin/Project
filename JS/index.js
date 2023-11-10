febae81e569b2faad2e9ebe270fe43da174466bd
Shamil Mubarakshin <shamilmubarakshin@Shamils-MacBook-Pro.local>
2023-11-10 10:43:01 -0500

    update repo with JS stuff

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+document.write('<h1>', 'Hello World', '<h1>');
+
+
+let name = 'it-incubator';
+let graduatesCount = 2000;
+let areYouChampion = true;
+
+document.write('<h1>', name, '<h1>');
+document.write('<input type="number" value="' + graduatesCount + '">');
+document.write('<input type="checkbox"', areYouChampion ? "checked" : "", '>');
